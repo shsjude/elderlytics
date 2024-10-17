c19d8e5b0aed69de23cdd3e67f8566f4e466a36e
Jude Adenuga <jude.adenuga@safeharborsolutionsllc.com>
2024-10-17 10:42:00 -0700

    Increase Node.js memory limit to 9GB and other changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';  // Use HashRouter for GitHub Pages
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';  // Import the global styles
-import Dashboard from './pages/Dashboard';
-import FacilityProfile from './pages/FacilityProfile';
 import NavbarComponent from './components/NavbarComponent';
 import FooterComponent from './components/FooterComponent';
+
+// Lazy load components
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const FacilityProfile = lazy(() => import('./pages/FacilityProfile'));
 
 const App: React.FC = () => {
   const [showSidebar, setShowSidebar] = useState<boolean>(true); // Manage sidebar state at the App level
@@ -13,14 +15,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Router> {/* Wrap the app in HashRouter */}
-        {/* Single Navbar and Footer at App level */}
         <NavbarComponent onOpenSidebar={() => setShowSidebar(!showSidebar)} />
         <div className="app-content">
-          <Routes>
-            {/* Pass the showSidebar state as a prop to both Dashboard and FacilityProfile */}
-            <Route path="/" element={<Dashboard showSidebar={showSidebar} />} />
-            <Route path="/facility/:facilityId" element={<FacilityProfile showSidebar={showSidebar} />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>  {/* Suspense for lazy-loaded components */}
+            <Routes>
+              <Route path="/" element={<Dashboard showSidebar={showSidebar} />} />
+              <Route path="/facility/:facilityId" element={<FacilityProfile showSidebar={showSidebar} />} />
+            </Routes>
+          </Suspense>
         </div>
         <FooterComponent />
       </Router>
